7e91e0a055b6d14c3891e2aa2f4bafe432626e0f
Elias Gomes <eliasgo221@gmail.com>
2024-07-10 20:46:37 -0300

    Add app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// const add = require('./utils.js')
+
+// const sum = add(1,2)
+
+// console.log(sum)
+
+// Chalange
+const getNotes = require('./notes.js')
+
+const msg = getNotes()
+
+console.log(msg)
+
