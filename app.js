6f7ea30b3b78831464b3eb412f0c21e20f89ee4d
Elias Gomes <eliasgo221@gmail.com>
2024-07-15 16:44:48 -0300

    add packages

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-// const add = require('./utils.js')
+import chalk from 'chalk';
+import yargs from 'yargs';
+import getNotes from './notes.js';
 
-// const sum = add(1,2)
+const log = console.log;
 
-// console.log(sum)
 
-// Chalange
-const getNotes = require('./notes.js')
+yargs(process.argv)
+// Customize yargs version
+.version('1.1.1')
+// Creat add command 
+.command({
+    command: 'add', 
+    describe: 'Add a new note',
+    handler: function (){
+        log('Adding a new note')
+    }
+})
+// Create remove commmand 
+.command({
+    command: 'remove', 
+    describe: 'Remove a note',
+    handler: function (){
+        log('Removing the note')
+    }
+})
+// Create list commmand 
+.command({
+    command: 'list', 
+    describe: 'List the notes',
+    handler: function (){
+        log('Listing out all notes')
+    }
+})
+// Create read commmand 
+.command({
+    command: 'read', 
+    describe: 'Read a notes',
+    handler: function (){
+        log('Reading a notes')
+    }
+}).argv
 
-const msg = getNotes()
-
-console.log(msg)
-
+log(yargs(process.argv).argv);
